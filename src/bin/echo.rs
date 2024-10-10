9fb1175904ac7b4252197b22cc3df51f661e8ae6
Edison Neto <eaneto@posteo.com>
2024-10-09 21:38:55 -0300

    WIP: New HTTP parser
    TODO:
    - Parse request body
    - Handle cases where headers are bigger than 4k
    - A bunch of expects that should be correctly handled
    - Content-Length headers is required in this implementation, but in the
      http spec that's no true, i'll probably keep this until later on.

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,10 +1,10 @@
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
 use clap::Parser;
 use tokio::{
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::TcpListener,
-    time,
+    net::{TcpListener, TcpStream},
+    stream, time,
 };
 
 use tracing::{debug, info};
@@ -38,40 +38,7 @@ async fn main() {
                     let request_id = Uuid::new_v4();
                     info!("Received message, request_id={request_id}");
 
-                    let mut cursor = 0;
-                    let mut buf = vec![0u8; 4096];
-
-                    let CR = '\r';
-                    let LF = '\n';
-
-                    // TODO: https://www.rfc-editor.org/rfc/rfc2616#section-2
-                    // First line: VERB PATH PROTOCOL
-                    // Second line: Headers
-                    // Other line: Body
-                    loop {
-                        if buf.len() == cursor {
-                            buf.resize(cursor * 2, 0);
-                        }
-
-                        let bytes_read = match stream.read(&mut buf[cursor..]).await {
-                            Ok(size) => size,
-                            Err(e) => {
-                                debug!("Error reading TCP stream to parse command, request_id={request_id}, error={e}");
-                                break;
-                            }
-                        };
-
-                        if bytes_read == 0 {
-                            break;
-                        }
-
-                        cursor += bytes_read;
-
-                        // if reach_end(\r\n) {
-                        //}
-                    }
-
-                    debug!("{}", String::from_utf8_lossy(&buf));
+                    parse_request(&request_id, stream).await;
 
                     //let buf_reader = BufReader::new(&mut stream);
                     //let mut request_lines = buf_reader.lines();
@@ -119,6 +86,119 @@ async fn main() {
                 });
             }
             Err(_) => eprintln!("Error listening to socket"),
+        }
+    }
+}
+
+async fn parse_request(request_id: &uuid::Uuid, mut stream: TcpStream) {
+    // TODO: https://www.rfc-editor.org/rfc/rfc2616#section-2
+    //
+    // First line: METHOD PATH PROTOCOL
+    //
+    // Second line: Headers
+    //
+    // Other line: Body
+
+    let mut method = String::new();
+    let mut path = String::new();
+    let mut protocol = String::new();
+    let mut headers = HashMap::<String, String>::new();
+    let mut _body = String::new();
+
+    let cursor = 0;
+    let mut buf = vec![0u8; 4096];
+
+    let bytes_read = match stream.read(&mut buf[cursor..]).await {
+        Ok(size) => size,
+        Err(e) => {
+            debug!("Error reading TCP stream to parse command, request_id={request_id}, error={e}");
+            0
+        }
+    };
+
+    if bytes_read == 0 {
+        panic!("HELP");
+    }
+
+    debug!("{}", String::from_utf8_lossy(&buf));
+
+    const CR: u8 = 13;
+    const LF: u8 = 10;
+
+    // Parse request line
+    let mut initial_position = 0;
+    for i in 0..(buf.len() - 1) {
+        if buf[i] == CR && buf[i + 1] == LF {
+            let request_line = String::from_utf8_lossy(&buf[initial_position..i]);
+            let mut request_line = request_line.split_whitespace();
+            method = request_line
+                .next()
+                .expect("Where's the method?")
+                .to_string();
+            path = request_line.next().expect("Where's the path?").to_string();
+            protocol = request_line
+                .next()
+                .expect("Where's the protocol?")
+                .to_string();
+            initial_position = i + 2;
+            break;
+        }
+    }
+
+    debug!("Initial position: {initial_position}");
+    debug!("Method: {method}, Path: {path}, Protocol: {protocol}");
+
+    // Parse headers
+    let mut header_position = initial_position;
+    for i in initial_position..(buf.len() - 3) {
+        if buf[i] == CR && buf[i + 1] == LF {
+            // This means \r\n\r\n, which is the end of the headers
+            // and the beginning of the body(or the end of the
+            // request).
+            if buf[i + 2] == CR && buf[i + 3] == LF {
+                initial_position = i + 4;
+                break;
+            }
+
+            // Parse header line
+            let header_line = String::from_utf8_lossy(&buf[header_position..i]);
+            let mut header_line = header_line.split(":");
+            let key = header_line.next().expect("Header key missing").to_string();
+            let value = header_line
+                .next()
+                .expect("Header value missing")
+                .trim_start()
+                .to_string();
+            headers.insert(key, value);
+            header_position = i + 2;
+        }
+    }
+
+    debug!("Initial position: {initial_position}");
+
+    debug!("Headers:");
+    for (key, value) in headers.iter() {
+        debug!("Key: {key}, Value: {value}");
+    }
+
+    if initial_position >= bytes_read {
+        debug!("This is the end");
+    }
+
+    // content-length should be required if method is post:
+    if method == "POST" {
+        let content_length = headers
+            .get("Content-Length")
+            .expect("Content length should have been sent in the request");
+
+        let content_length = content_length
+            .parse::<u16>()
+            .expect("Content length should fit in a u16");
+
+        if bytes_read - initial_position >= content_length as usize {
+            debug!("I have read enough from the socket!");
+        } else {
+            debug!("I need to read more from the socket!");
         }
     }
 }
